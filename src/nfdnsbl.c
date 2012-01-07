14678a48216c119f5cddef9b54f7720b3be425d1
Simon Le Lann <simon.le-lann@dosadi.fr>
2012-01-07 19:36:18 +0100

    Placed option_destroy in atexit

--- src/nfdnsbl.c
+++ src/nfdnsbl.c
@@ -1,14 +1,21 @@
 #include "nfdnsbl_config.h"
 #include "config.h"
+#include <stdlib.h>
+
+option_t option;
+
+void exit_callback(void)
+{
+	destroy_option(&option);
+}
 
 int main()
 {
-	option_t option;
 	init_option(&option);
-	print_option(&option);
+	atexit(exit_callback);
 	read_config(&option,CONFFILE);
 	print_option(&option);
-	destroy_option(&option);
+
 	return 0;
 }
 
